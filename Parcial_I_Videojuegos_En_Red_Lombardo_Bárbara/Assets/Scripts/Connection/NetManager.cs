cf9abe0d649f7184e19e8e2a7afa621481fe19a0
Foster365 <31601427+Foster365@users.noreply.github.com>
2021-09-18 12:05:42 -0300

    Basic Prototype.
    + Basic prototype:
                       + NetManager.
                       + Character Basic Scripts.
                       + Instantiator.
                       + Photon Components attached to prefab.
                       +Menu and Game Scenes, with connection working properly.

---
+++ Parcial_I_Videojuegos_En_Red_Lombardo_Bárbara/Assets/Scripts/Connection/NetManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class NetManager : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField]
+    Button buttonConnect;
+    [SerializeField]
+    InputField inputFieldRoom;
+    [SerializeField]
+    InputField inputFieldNickName;
+    [SerializeField]
+    InputField inputFieldPlayersNumber;
+
+    //List<string> nickNames;
+
+    private void Start()
+    {
+
+        buttonConnect.interactable = false;
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public override void OnConnectedToMaster() //1
+    {
+        Debug.Log("Connected to master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby() //2
+    {
+        print("Connected to lobby");
+        buttonConnect.interactable = true;
+    }
+
+
+    public void Connect()
+    {
+
+        if (string.IsNullOrWhiteSpace(inputFieldRoom.text) || string.IsNullOrEmpty(inputFieldRoom.text)) return;
+        if (string.IsNullOrWhiteSpace(inputFieldNickName.text) || string.IsNullOrEmpty(inputFieldNickName.text)) return;
+
+        PhotonNetwork.NickName = inputFieldNickName.text;
+
+        RoomOptions options = new RoomOptions();
+        options.IsOpen = true;
+        options.IsVisible = true;
+
+        byte maxPlayers;
+
+        if (byte.TryParse(inputFieldRoom.text, out maxPlayers)) options.MaxPlayers = maxPlayers;
+        else options.MaxPlayers = 10;
+
+        string roomName = "";
+        if (string.IsNullOrEmpty(inputFieldRoom.text) || string.IsNullOrWhiteSpace(inputFieldRoom.text))
+        {
+            roomName = "La Cueva del Marcianeke";
+        }
+        else
+        {
+            roomName = inputFieldRoom.text;
+        }
+        PhotonNetwork.JoinOrCreateRoom(roomName, options, TypedLobby.Default);
+
+        buttonConnect.interactable = false;
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game"); //Usar esta función y no la de unity, porque la carga de manera local
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        buttonConnect.interactable = true;
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        buttonConnect.interactable = true;
+    }
+
+}
---
+++ Parcial_I_Videojuegos_En_Red_Lombardo_Bárbara/Assets/Scripts/Instantiator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Instantiator : MonoBehaviour
+{
+
+    public string prefabName;
+    private void Start()
+    {
+        PhotonNetwork.Instantiate(prefabName, new Vector3(0, .5f, 0), Quaternion.identity);
+    }
+}
