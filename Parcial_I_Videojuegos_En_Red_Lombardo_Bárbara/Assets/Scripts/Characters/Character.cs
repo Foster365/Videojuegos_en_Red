89f58d69ba62927abccf03d82fee65ee37799131
Foster365 <31601427+Foster365@users.noreply.github.com>
2021-09-20 01:34:22 -0300

    Point Counters, Balls (Points), Level Timer

--- Parcial_I_Videojuegos_En_Red_Lombardo_Bárbara/Assets/Scripts/Characters/Character.cs
+++ Parcial_I_Videojuegos_En_Red_Lombardo_Bárbara/Assets/Scripts/Characters/Character.cs
@@ -8,12 +8,16 @@ public class Character : MonoBehaviour //No uso Player porque Photon tiene una c
     float speed;
     [SerializeField]
     float jumpForce;
+    int score;
 
     Rigidbody rbody;
+
+    public int Score { get => score; set => score = value; }
 
     private void Awake()
     {
         rbody = GetComponent<Rigidbody>();
+        score = 0;
     }
 
     public void Move(Vector3 dir)
--- Parcial_I_Videojuegos_En_Red_Lombardo_Bárbara/Assets/Scripts/Managers/GameManager.cs
+++ Parcial_I_Videojuegos_En_Red_Lombardo_Bárbara/Assets/Scripts/Managers/GameManager.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] float maxGameTimer;
+    [SerializeField] Text textGameCounter;
+    GameObject[] winners;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        textGameCounter.text = maxGameTimer.ToString();
+
     }
 
     // Update is called once per frame
@@ -22,15 +25,22 @@ public class GameManager : MonoBehaviour
     void GameTimer()
     {
 
-        float timer = 0;
-
-        timer += Time.deltaTime;
-        if (timer >= maxGameTimer) EndGame();
+        maxGameTimer -= Time.deltaTime;
+        textGameCounter.text = maxGameTimer.ToString();
+        if (maxGameTimer <= 0) EndGame();
 
     }
 
     void EndGame()
     {
+        winners = GameObject.FindGameObjectsWithTag("Character");
+
+        for (int i = winners.Length -1; i >= 0; i--)
+        {
+            int winScore = winners[i].GetComponent<Character>().Score;
+            if (winScore < winners[i].GetComponent<Character>().Score) winScore = winners[i].GetComponent<Character>().Score;
+        }
+
         //Ver si cuenta automáticamente los puntos de cada uno.
         //Podría llevar a los players a otra sala o en la misma, mostrar los puntos y compararlos, mostrando al ganador.
     }
