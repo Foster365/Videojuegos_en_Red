fe3a7de7735a48f5fac70c564084ecb29c56b883
Bárbara Lucia Lombardo <barbara.lombardo.98@gmail.com>
2022-08-22 23:24:15 -0300

    fix: Folder organizing

---
+++ Parcial_I/Parcial_I_Videojuegos_en_red_Lombardo_Barbara/Assets/Model/Character.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed;
+
+    Rigidbody _rigidbody;
+    GameObject projectile;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 dir)
+    {
+        dir = dir.normalized;
+        dir *= movementSpeed;
+        dir.y = _rigidbody.velocity.y;
+        _rigidbody.velocity = dir;
+    }
+
+    public void Shoot()
+    {
+        GameObject.Instantiate(projectile, transform.position, Quaternion.identity);
+    }
+
+}
